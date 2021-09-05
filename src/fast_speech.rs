f3c5a53d9e0fff17c28142fe97f2a948e8fa8178
andompesta <ando.cavallari@gmail.com>
2021-09-05 19:27:06 +0200

    add read lexicon

--- src/fast_speech.rs
+++ src/fast_speech.rs
@@ -1,7 +1,53 @@
 use tch;
-use std::path::Path;
+use std::collections::HashMap;
+use std::fs;
+use std::io::{BufRead, BufReader};
+use regex::Regex;
 
-pub fn load_model() -> Result<tch::CModule, tch::TchError> {
-    let path = Path::new("./resources/traced.pt");
-    return tch::CModule::load(path);
+pub fn load_model(path: &str) -> tch::CModule {
+    let model = match tch::CModule::load(path) {
+        Ok(model) => model,
+        Err(e) => {
+            panic!("{}", e);
+        }
+    };
+
+    return model;
+}
+
+
+pub struct PhonemsProcessor {
+    lexicon: HashMap<String, String>
+}
+
+impl PhonemsProcessor {
+    pub fn build(path: &str) -> PhonemsProcessor {
+        let file = fs::File::open(path).unwrap();
+        let reader = BufReader::new(file);
+        let mut lexicon = HashMap::new();
+        let seperator = Regex::new(r"\s+").unwrap();
+
+        for (index, line) in reader.lines().enumerate() {
+            let line = line.unwrap();
+            let line = line.trim();
+            let v: Vec<&str> = seperator.splitn(line, 2).collect();
+            
+            let word = v[0].trim().to_lowercase().to_string();
+            let phonem = v[1].trim().to_string();
+
+            if ! lexicon.contains_key(&word) {
+                lexicon.insert(word, phonem);
+            }
+        }
+
+        return PhonemsProcessor{lexicon:lexicon};
+    }
+
+    pub fn len(&self) -> usize {
+        return self.lexicon.len();
+    }
+
+    pub fn get(&self, key: &str) -> Option<&String> {
+        return self.lexicon.get(key);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
+#![allow(unused_variables)]
 use rust_arxiv_podcast::{arxiv};
 use rust_arxiv_podcast::{fast_speech};
+
 
 #[tokio::main]
 async fn main() {
 
-    let model = fast_speech::load_model().expect(
-        "error loading the model"
+    let processor = fast_speech::PhonemsProcessor::build(
+        "./resources/librispeech-lexicon.txt"
     );
 
-    let arxiv_body = arxiv::get_body().await.expect(
-        "error in the request of today"
-    );
+    println!("{}", processor.len());
+    println!("{}", processor.get("a").unwrap());
+    
+    let model = fast_speech::load_model("./resources/traced.pt");
+
+    let arxiv_body = match arxiv::get_body().await {
+        Ok(body) => body,
+        Err(e) => panic!("error in the request of today \t {}", e)
+    };
 
     let feed = arxiv::parse(&arxiv_body[..]).expect(
         "Error in parsing the xlm body"
